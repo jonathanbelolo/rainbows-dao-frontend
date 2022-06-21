2615238b5984b033386b87814133316ad51983bf
jonathan <jonathan@scorpiomusic.fr>
2022-06-21 23:37:24 +0200

    added moralis and stuff

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
 import '../styles/globals.css'
+import { MoralisProvider } from "react-moralis"
+
+const APP_ID = process.env.NEXT_PUBLIC_APP_ID
+const SERVER_URL = process.env.NEXT_PUBLIC_SERVER
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <MoralisProvider appId={APP_ID} serverUrl={SERVER_URL} initializeOnMount={true}>
+        <Component {...pageProps} />
+      </MoralisProvider>
+    </>
+  
+  )
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { ConnectButton } from 'web3uikit'
 
 export default function Home() {
   return (
@@ -15,7 +16,10 @@ export default function Home() {
       <main>
         <h1> 
           Rainbows DAO
-        </h1>        
+        </h1>      
+        <nav>
+          <ConnectButton moralisAuth={false}/>
+        </nav>  
       </main>
 
     </div>
